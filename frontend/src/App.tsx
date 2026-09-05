811e14e63eb50a2d76be10b34705e5cfc4f63c76
Muneer Mohamed <22321258@student.ciu.edu.tr>
2026-09-05 18:11:53 +0300

    Frontend configurations

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <h1 className="p-8 text-3xl font-bold text-gray-900">
+        PDF Editor
+      </h1>
+    </div>
+  )
+}
+
+export default App
